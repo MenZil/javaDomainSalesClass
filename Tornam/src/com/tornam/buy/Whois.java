467b065e440fdc223aea73ba3e0d49954e8f2c7e
MenZil <nusret081@gmail.com>
2015-06-22 21:36:15 +0800

    tornam ni qoshtum

---
+++ Tornam/src/com/tornam/buy/Whois.java
@@ -0,0 +1,55 @@
+package com.tornam.buy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Whois {
+
+	private static final int DEFAULT_PORT = 43;
+	
+	public String query(String domain) throws Exception {
+		String server = "";
+		String tld = getTLD(domain);
+		if ("com".equals(tld)) {
+			server = "whois.verisign-grs.com";
+		} else if ("net".equals(tld)) {
+			server = "whois.verisign-grs.com";
+		} else if ("org".equals(tld)) {
+			server = "whois.pir.org";
+		} else if ("cn".equals(tld)) {
+			server = "whois.cnnic.cn";
+		} else if ("jp".equals(tld)) {
+			server = "whois.jprs.jp";
+		} else if ("kr".equals(tld)) {
+			server = "whois.kr";
+		}
+		return query(domain, server);
+	}
+	
+	public String query(String domain, String server) throws Exception {
+		Socket socket = new Socket(server, DEFAULT_PORT);
+		String lineSeparator = "\r\n";
+
+		PrintWriter out = new PrintWriter(socket.getOutputStream());
+		out.println(domain);
+		out.flush();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		StringBuilder ret = new StringBuilder();
+		String line;
+		while ((line = in.readLine()) != null) {
+			ret.append(line + lineSeparator);
+		}
+		socket.close();
+		return ret.toString();
+	}
+	
+	private String getTLD(String domain) {
+		final int index;
+		return (domain == null || (index = domain.lastIndexOf('.') + 1) < 1) ? domain
+				: (index < (domain.length())) ? domain.substring(index) : "";
+	}
+	
+}
